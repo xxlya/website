96b0b4c09bbdcec93ea64fb6f48c186b29430e9a
Xiaoxiao Li <xiaoxiao.li@aya.yale.edu>
2022-07-18 01:26:43 -0700

    Updates

---
+++ precache-manifest.fadd128a340b3f8d015dd880c3f0aec2.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "329f67a7a5af751cbac2",
+    "url": "/lab-web/static/css/main.0a224e27.chunk.css"
+  },
+  {
+    "revision": "329f67a7a5af751cbac2",
+    "url": "/lab-web/static/js/main.329f67a7.chunk.js"
+  },
+  {
+    "revision": "ad39ee43e5f1e39ec27e",
+    "url": "/lab-web/static/js/1.ad39ee43.chunk.js"
+  },
+  {
+    "revision": "99aabfd7b72eeea10c91",
+    "url": "/lab-web/static/js/2.99aabfd7.chunk.js"
+  },
+  {
+    "revision": "fd6a2337bedeb95726c9",
+    "url": "/lab-web/static/js/runtime~main.fd6a2337.js"
+  },
+  {
+    "revision": "b4e66b8f9de99890eed167c0bf472349",
+    "url": "/lab-web/static/media/xiaoxiao.b4e66b8f.jpg"
+  },
+  {
+    "revision": "4cdacb8f89d588d69e8570edcbe49507",
+    "url": "/lab-web/static/media/Poppins-SemiBold.4cdacb8f.ttf"
+  },
+  {
+    "revision": "544fa4f2678a8285eb88b8dfe503c90c",
+    "url": "/lab-web/static/media/Poppins-ExtraBold.544fa4f2.ttf"
+  },
+  {
+    "revision": "a3e0b5f427803a187c1b62c5919196aa",
+    "url": "/lab-web/static/media/Poppins-Bold.a3e0b5f4.ttf"
+  },
+  {
+    "revision": "f6ea751e936ade6edcd03a26b8153b4a",
+    "url": "/lab-web/static/media/Poppins-Light.f6ea751e.ttf"
+  },
+  {
+    "revision": "25cd0f688f815bc4f6ac2b71eb6278ba",
+    "url": "/lab-web/static/media/Poppins-Thin.25cd0f68.ttf"
+  },
+  {
+    "revision": "f61a4eb27371b7453bf5b12ab3648b9e",
+    "url": "/lab-web/static/media/Poppins-Medium.f61a4eb2.ttf"
+  },
+  {
+    "revision": "8b6af8e5e8324edfd77af8b3b35d7f9c",
+    "url": "/lab-web/static/media/Poppins-Regular.8b6af8e5.ttf"
+  },
+  {
+    "revision": "07d5078ce17862c9758d70081c3b5b77",
+    "url": "/lab-web/static/media/bg.07d5078c.png"
+  },
+  {
+    "revision": "98c402fbc4e76460282966610d4a9d74",
+    "url": "/lab-web/static/media/fedbn.98c402fb.png"
+  },
+  {
+    "revision": "387b52923829d9bca9ec2d5f5345bf28",
+    "url": "/lab-web/static/media/gate.387b5292.png"
+  },
+  {
+    "revision": "d10bd983bd78e2706bde103a20002e63",
+    "url": "/lab-web/static/media/class_impression.d10bd983.png"
+  },
+  {
+    "revision": "d0df94565d2e98bd9d8d4c9c13155afb",
+    "url": "/lab-web/static/media/examine.d0df9456.png"
+  },
+  {
+    "revision": "60d2f7b56984cc1bfcdf6e78b68b61f4",
+    "url": "/lab-web/static/media/nserc.60d2f7b5.png"
+  },
+  {
+    "revision": "eb2285c6921055fe040d35e7a5be1f57",
+    "url": "/lab-web/static/media/compute_canada.eb2285c6.png"
+  },
+  {
+    "revision": "b8bb239773091abf28d557065e193496",
+    "url": "/lab-web/static/media/cifar.b8bb2397.png"
+  },
+  {
+    "revision": "aca3ef6429457bbbffef6c410d61a7c1",
+    "url": "/lab-web/static/media/nvidia.aca3ef64.png"
+  },
+  {
+    "revision": "25b25c93e1c9c5739eb0cd0e871039e0",
+    "url": "/lab-web/static/media/roche.25b25c93.png"
+  },
+  {
+    "revision": "5cfc1ec5c501f081040760def93b7e86",
+    "url": "/lab-web/static/media/azure.5cfc1ec5.png"
+  },
+  {
+    "revision": "34611d511a62598f2b8fcf86f531d466",
+    "url": "/lab-web/static/media/meta.34611d51.png"
+  },
+  {
+    "revision": "2f1a17a3bfa874a1ac27fbcf4211bf7e",
+    "url": "/lab-web/static/media/logo_transparent_background.2f1a17a3.png"
+  },
+  {
+    "revision": "bbe95147e3b5566491ef9fe6c45577b1",
+    "url": "/lab-web/index.html"
+  }
+];
